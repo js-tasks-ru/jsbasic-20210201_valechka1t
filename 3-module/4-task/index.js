c789120a29ae5f58c1efcba881d5a8f7cd76c8d4
Valentina Antanovich <valentina8antanovich@gmail.com>
2021-02-15 01:40:17 +0300

    task 3.4

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,10 @@
 function showSalary(users, age) {
-  // ваш код...
+  let salaryFilter = [];
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].age <= age) {
+      salaryFilter.push(users[i].name, ", ", users[i].balance, "\n") 
+    }
+  }
+  let str = salaryFilter.join('');
+  return str.trim();
 }
