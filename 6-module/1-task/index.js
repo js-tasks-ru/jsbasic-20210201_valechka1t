581cd68f5a89a82edc2dfa3adc5d026d52423356
Valentina Antanovich <valentina8antanovich@gmail.com>
2021-03-11 02:22:16 +0300

    task 6.1

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,26 +14,33 @@
  */
 export default class UserTable {
   constructor(rows) {
+    // this.rows = rows;
     let table = document.createElement('table');
     let thead = document.createElement('thead');
-    thead.innerHTML = <tr><th>Name</th><th>Age</th><th>Salary</th><th>City</th></tr>;
+    thead.innerHTML = `<tr><th>Name</th><th>Age</th><th>Salary</th><th>City</th></tr>`;
     table.appendChild(thead);
     let tbody = document.createElement('tbody');
     table.appendChild(tbody);
     for (let i=0; i < rows.length; i++) {
       let tr = document.createElement('tr');
 
-      for (let j=0; j < rows[i].length + 1; j++) {
+        let name = document.createElement('td');
+        tr.appendChild(name);
+        name.innerHTML = rows[i].name;
+        let age = document.createElement('td');
+        tr.appendChild(age);
+        age.innerHTML = rows[i].age;
+        let salary = document.createElement('td');
+        tr.appendChild(salary);
+        salary.innerHTML = rows[i].salary;
+        let city = document.createElement('td');
+        tr.appendChild(city);
+        city.innerHTML = rows[i].city;
         let td = document.createElement('td');
         tr.appendChild(td);
-        td.innerHTML = row[i][j];
-        if (j === rows[i].length + 1) {
-          let button = document.createElement('button'); 
-          td.appendChild(button);
-          button.innerHTML = `[X]`;
-          }
-      }
-      table.appendChild(tr);
+        td.innerHTML = `<button>[X]</button>`;
+
+      tbody.appendChild(tr);
     }
     table.addEventListener("click", (event) => event.target.closest('tr').remove());
 
