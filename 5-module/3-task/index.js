d2c16179f36059e209be5242c520b755753c7d4e
Valentina Antanovich <valentina8antanovich@gmail.com>
2021-03-11 01:38:30 +0300

    task 5.3

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,35 +1,38 @@
 function initCarousel() {
+  let position = 0;
   let caruselLine = document.querySelector('[class="carousel__inner"]');
   let caruselArrowRight = document.querySelector('[class="carousel__arrow carousel__arrow_right"]');
   let caruselArrowLeft = document.querySelector('[class="carousel__arrow carousel__arrow_left"]');
-  let caruselImageNumber = document.querySelectorAll('div.carousel__slide').length;
+  let caruselImage = document.querySelectorAll('div.carousel__slide');
+  let caruselImageNumber = caruselImage.length;
   let caruselImageWidth = document.querySelector('[class="carousel__slide"]').offsetWidth;
-  let computedtransformStyle = getComputedStyle(caruselLine)
-  console.log (caruselLine);  
-  console.log (caruselArrowRight);
-  console.log (caruselImageNumber);
-  console.log (caruselImageWidth);
-  console.log (computedtransformStyle.offsetWidth); 
-
+  
+  checkArrow (position);
 
   caruselArrowRight.addEventListener("click", function() {
     
+    let imagesLeft = caruselImageNumber - (Math.abs(position) + caruselImageWidth)/ caruselImageWidth;
+    position -= imagesLeft >= 1 ? caruselImageWidth : imagesLeft * caruselImageWidth;
     
-    //for (let i = 1; i < (caruselImageNumber + 1); i++) {
-      caruselLine.style.transform = 'translateX(-300%)';
-
-    //}
-    
-    //if (caruselLine.scrollLeft == 0) {
-      //carouselArrowRight.style.display = 'none';
-   //}
-  });
+    caruselLine.style.transform = `translateX(${position}px)`;
+    checkArrow ();
+  });  
 
   caruselArrowLeft.addEventListener("click", function() {
-    caruselLine.style.transform = 'translateX(100%)';
-    if (caruselLine.scrollLeft == caruselImageWidth) {
-      carouselArrowLeft.style.display = 'none';
-    }
+    
+    let imagesLeft = Math.abs(position) / caruselImageWidth;
+    position += imagesLeft >= 1 ? caruselImageWidth : imagesLeft * caruselImageWidth;
+    
+    caruselLine.style.transform = `translateX(${position}px)`;
+    checkArrow ();    
   });
-}
 
+  function checkArrow () {
+    if (position <= -(caruselImageNumber - 1) * caruselImageWidth) {
+      caruselArrowRight.style.display = 'none';
+    } else {caruselArrowRight.style.display = '';}
+    if (position === 0) {
+      caruselArrowLeft.style.display = 'none';
+    } else {caruselArrowLeft.style.display = '';}
+  }
+}
--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,29 @@
  */
 export default class UserTable {
   constructor(rows) {
+    let table = document.createElement('table');
+    let thead = document.createElement('thead');
+    thead.innerHTML = <tr><th>Name</th><th>Age</th><th>Salary</th><th>City</th></tr>;
+    table.appendChild(thead);
+    let tbody = document.createElement('tbody');
+    table.appendChild(tbody);
+    for (let i=0; i < rows.length; i++) {
+      let tr = document.createElement('tr');
+
+      for (let j=0; j < rows[i].length + 1; j++) {
+        let td = document.createElement('td');
+        tr.appendChild(td);
+        td.innerHTML = row[i][j];
+        if (j === rows[i].length + 1) {
+          let button = document.createElement('button'); 
+          td.appendChild(button);
+          button.innerHTML = `[X]`;
+          }
+      }
+      table.appendChild(tr);
+    }
+    table.addEventListener("click", (event) => event.target.closest('tr').remove());
+
+    this.elem = table;
   }
 }
